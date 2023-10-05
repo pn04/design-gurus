f3071b5cfc6e9b7ce3aed97e503269b9fddfa75d
pn04 <33034498+pn04@users.noreply.github.com>
2023-10-06 01:09:17 +0530

    solved some more problems

---
+++ src/design-gurus/TwoPointers.cs
@@ -0,0 +1,238 @@
+﻿namespace design_gurus;
+
+//https://leetcode.com/problems/two-sum/
+public class TwoPointers
+{
+    //nums = [2,7,11,15], target = 9 => [0,1]
+    public int[] TwoSum(int[] nums, int target)
+    {
+        // int[] pair = new int[2];
+        // for (int i = 0; i < nums.Length; i++)
+        // {
+        //     var compliment = target - nums[i];
+        //     var pairItem = Array.LastIndexOf(nums, compliment);
+        //     if (pairItem != -1 && pairItem != i)
+        //     {
+        //         pair[0] = i;
+        //         pair[1] = pairItem;
+        //         return pair;
+        //     }
+        // }
+        // return pair;
+        Dictionary<int, int> compliments = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var currComplement = target - nums[i];
+            if (compliments.ContainsKey(currComplement))
+            {
+                return new int[] { compliments[currComplement], i };
+            }
+            else
+            {
+                compliments[nums[i]] = i;
+            }
+        }
+        throw new InvalidOperationException();
+    }
+
+    public int[] TwoSumSorted(int[] arr, int target)
+    {
+        int left = 0, right = arr.Length - 1;
+        while (left <= right)
+        {
+            if (arr[left] + arr[right] < target)
+            {
+                left++;
+            }
+            else if (arr[left] + arr[right] > target)
+            {
+                right--;
+            }
+            else
+            {
+                return new int[2] { left, right };
+            }
+        }
+        return new int[2] { -1, -1 };
+    }
+
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+    // 2,3,3,3,6,9,9 => 2,3,6,3,3,9,9
+    // 2,2,2,3,4,5,5 
+    public int RemoveDuplicates(int[] nums)
+    {
+        int nextUnique = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[nextUnique - 1])
+            {
+                nums[nextUnique++] = nums[i];
+            }
+        }
+        return nextUnique;
+    }
+
+    //-3, -1, 0, 1, 2 -> 0, 1, 1, 4, 9
+    public int[] SortedSquares(int[] nums)
+    {
+        // int firstPos = 1; //got this from bin search
+        // int negPos = firstPos - 1;
+        // int[] res = new int[nums.Length];
+
+        // for (int i = 0; i < res.Length; i++)
+        // {
+        //     if (firstPos < nums.Length && negPos >= 0)
+        //     {
+        //         int posSquare = nums[firstPos] * nums[firstPos];
+        //         int negSquare = nums[negPos] * nums[negPos];
+        //         if (posSquare < negSquare)
+        //         {
+        //             res[i] = posSquare;
+        //             firstPos++;
+        //         }
+        //         else
+        //         {
+        //             res[i] = negSquare;
+        //             negPos--;
+        //         }
+        //     }
+        //     else if (firstPos >= nums.Length)
+        //     {
+        //         res[i] = nums[negPos] * nums[negPos];
+        //         negPos++;
+        //     }
+        //     else
+        //     {
+        //         res[i] = nums[firstPos] * nums[firstPos];
+        //         firstPos++;
+        //     }
+        // }
+        // return res;
+        int[] result = new int[nums.Length];
+        int largestSquareIndex = nums.Length - 1;
+        int left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare)
+            {
+                result[largestSquareIndex] = leftSquare;
+                left++;
+            }
+            else
+            {
+                result[largestSquareIndex] = rightSquare;
+                right--;
+            }
+            largestSquareIndex--;
+        }
+        return result;
+    }
+
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        IList<IList<int>> triplets = new List<IList<int>>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            SearchPair(nums, -nums[i], i + 1, triplets);
+        }
+        return triplets;
+    }
+
+    private void SearchPair(int[] arr, int target, int left, IList<IList<int>> triplets)
+    {
+        int right = arr.Length - 1;
+        while (left < right)
+        {
+            if (arr[left] + arr[right] == target)
+            {
+                triplets.Add(new int[3] { -target, arr[left], arr[right] });
+                left++;
+                right--;
+                //skip duplicates
+                while (left < right && arr[left] == arr[left - 1])
+                {
+                    left++;
+                }
+                while (left < right && arr[right] == arr[right + 1])
+                {
+                    right--;
+                }
+            }
+            else if (arr[left] + arr[right] < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    }
+
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int minSumTriplet = int.MaxValue;
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            int left = i + 1, right = nums.Length - 1;
+            while (left < right)
+            {
+                int currentSum = nums[i] + nums[left] + nums[right];
+                if (currentSum == target)
+                {
+                    return currentSum;
+                }
+                else
+                {
+                    if (Math.Abs(currentSum - target) < Math.Abs(minSumTriplet - target)
+                       || (Math.Abs(currentSum - target) == Math.Abs(minSumTriplet - target)) && currentSum < minSumTriplet)
+                    {
+                        minSumTriplet = currentSum;
+                    }
+
+                    if (currentSum < target)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+        }
+        return minSumTriplet;
+    }
+
+    public int ThreeSumSmaller(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int tripletCount = 0;
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            int left = i + 1, right = nums.Length - 1;
+            while (left < right)
+            {
+                if (nums[i] + nums[left] + nums[right] < target)
+                {
+                    tripletCount += right - left;
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+        return tripletCount;
+    }
+}
---
+++ tests/design-gurus-tests/TwoPointersTests.cs
@@ -0,0 +1,221 @@
+﻿using design_gurus;
+
+namespace design_gurus_tests;
+
+public class TwoPointersTests
+{
+    //write a test for TwoSum
+
+    [Fact]
+    public void TwoSumTest()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 2, 7, 11, 15 };
+        var target = 9;
+        //act
+        var result = twoPointers.TwoSum(nums, target);
+        //assert
+        Assert.Equal(new int[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void TwoSumTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 2, 7, 11, 15 };
+        var target = 26;
+        //act
+        var result = twoPointers.TwoSum(nums, target);
+        //assert
+        Assert.Equal(new int[] { 2, 3 }, result);
+    }
+
+    [Fact]
+    public void TwoSumSortedTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 1, 2, 3, 4, 6 };
+        var target = 6;
+        //act
+        var result = twoPointers.TwoSumSorted(nums, target);
+        //assert
+        Assert.Equal(new int[] { 1, 3 }, result);
+    }
+
+    [Fact]
+    public void TwoSumSortedTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 2, 5, 9, 11 };
+        var target = 11;
+        //act
+        var result = twoPointers.TwoSumSorted(nums, target);
+        //assert
+        Assert.Equal(new int[] { 0, 2 }, result);
+    }
+
+    [Fact]
+    public void RemoveDuplicatesTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 2, 3, 3, 3, 6, 9, 9 };
+        //act
+        var result = twoPointers.RemoveDuplicates(nums);
+        //assert
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void RemoveDuplicatesTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 2, 2, 2, 11 };
+        //act
+        var result = twoPointers.RemoveDuplicates(nums);
+        //assert
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void SortedSquaresTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -2, -1, 0, 2, 3 };
+        //act
+        var result = twoPointers.SortedSquares(nums);
+        //assert
+        Assert.Equal(new int[] { 0, 1, 4, 4, 9 }, result);
+    }
+
+    [Fact]
+    public void SortedSquaresTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -3, -1, 0, 1, 2 };
+        //act
+        var result = twoPointers.SortedSquares(nums);
+        //assert
+        Assert.Equal(new int[] { 0, 1, 1, 4, 9 }, result);
+    }
+
+    [Fact]
+    public void SortedSquaresTest3()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -5, -3, -2, -1 };
+        //act
+        var result = twoPointers.SortedSquares(nums);
+        //assert
+        Assert.Equal(new int[] { 1, 4, 9, 25 }, result);
+    }
+
+    [Fact]
+    public void SortedSquaresTest4()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -1, 0 };
+        //act
+        var result = twoPointers.SortedSquares(nums);
+        //assert
+        Assert.Equal(new int[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void ThreeSumTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -3, -1, 0, 1, 2 };
+        //act
+        var result = twoPointers.ThreeSum(nums);
+        //assert
+        Assert.Equal(new List<List<int>> { new List<int> { -3, 1, 2 }, new List<int> { -1, 0, 1 } }, result);
+    }
+
+    [Fact]
+    public void ThreeSumTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -5, 2, -1, -2, 3 };
+        //act
+        var result = twoPointers.ThreeSum(nums);
+        //assert
+        Assert.Equal(new List<List<int>> { new List<int> { -5, 2, 3 }, new List<int> { -2, -1, 3 } }, result);
+    }
+
+    [Fact]
+    public void ThreeSumClosestTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -2, 0, 1, 2 };
+        var target = 2;
+        //act
+        var result = twoPointers.ThreeSumClosest(nums, target);
+        //assert
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void ThreeSumClosestTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -3, -1, 1, 2 };
+        var target = 1;
+        //act
+        var result = twoPointers.ThreeSumClosest(nums, target);
+        //assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void ThreeSumClosestTest3()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { 1, 0, 1, 1 };
+        var target = 100;
+        //act
+        var result = twoPointers.ThreeSumClosest(nums, target);
+        //assert
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void ThreeSumSmallerTest1()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -2, 0, 1, 3 };
+        var target = 2;
+        //act
+        var result = twoPointers.ThreeSumSmaller(nums, target);
+        //assert
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void ThreeSumSmallerTest2()
+    {
+        //arrange
+        var twoPointers = new TwoPointers();
+        var nums = new int[] { -1, 4, 2, 1, 3 };
+        var target = 5;
+        //act
+        var result = twoPointers.ThreeSumSmaller(nums, target);
+        //assert
+        Assert.Equal(4, result);
+    }
+}
